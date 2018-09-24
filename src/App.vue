0e70854d750e87a4d73bea71e1784130f162c733
MikaelFeher <mikael.feher@gmail.com>
2018-09-24 12:59:24 +0200

    WIP - continued: Adds firebase and reworks the project to work with firebase.

--- src/App.vue
+++ src/App.vue
@@ -20,16 +20,15 @@
         <button type="submit" class="btn waves-effect waves-light green">Uppdatera varan</button>
       </form>
     </div>
-    groceries: {{ groceries.length }}
-    completedGroceries: {{ completedGroceries.length }}
+    <!-- List of not completed groceries -->
     <div v-if="!allCompleted" class="row">
       <h5>Varor</h5>
       <hr class="col s12 col m6 offset-m3">
       <draggable v-model="groceries" @change="updateOrder">
         <transition-group>
-          <ul v-if="groceries.length !== 0" v-for="item in groceries" :key="item['.key']" class="col s12 col m6 offset-m3">
+          <ul v-for="item in groceries" :key="item['.key']" class="col s12 col m6 offset-m3">
             <li v-if="!item.completed" class="left-align black-text "> 
-              <p class="col s10 truncate" @click="setItemCompleted(item._id)"><b>{{ item.name }}</b></p> 
+              <p class="col s10 truncate" @click="setItemCompleted(item['.key'])"><b>{{ item.name }}</b></p> 
               <i class="material-icons col s1 blue-grey-text" @click="editItem(item)">edit</i>
               <i class="material-icons col s1 red-text" @click="removeItem(item['.key'])">delete</i>
             </li>
@@ -38,17 +37,19 @@
       </draggable>
       <hr class="col s12 col m6 offset-m3">
     </div>
+    <!-- All done notification -->
     <div v-if="allCompleted" class="row">
       <div class="col s12 col m6 offset-m3 green">
         <h4 class="white-text">Du är klar :)))</h4>
       </div>
     </div>
-    <div v-if="completedGroceries.length !== 0" >
+    <!-- List of completed groceries -->
+    <div v-if="anyCompleted" >
       <div class="row">
         <h5>Plockade varor</h5>
         <hr class="col s12 col m6 offset-m3">
-        <ul v-for="item in completedGroceries" :key="item._id" class="col s12 col m6 offset-m3">
-          <li class="center-align black-text" @click="setItemNotCompleted(item._id)"> 
+        <ul v-for="item in groceries" :key="item['.key']" class="col s12 col m6 offset-m3">
+          <li v-if="item.completed" class="center-align black-text" @click="setItemNotCompleted(item['.key'])"> 
             <p class="col s12 truncate">{{ item.name }}</p> 
           </li>
         </ul>
@@ -61,7 +62,7 @@
 
 <script>
   import draggable from 'vuedraggable'
-  import { dbGroceriesRef, dbCompletedGroceriesRef } from './firebase/init.js'
+  import { dbGroceriesRef } from './firebase/init.js'
 
   export default {
     name: 'app',
@@ -71,37 +72,26 @@
     data() {
       return {
         itemToAdd: '',
-        completedGroceries: [],
+        anyCompleted: false,
         allCompleted: false,
         itemToEdit: {},
         isEditing: false,
         errorMsg: ''
       }
     },
-    created: function() {
-      this.groceries.map(item => console.log('item.name: ', item.name))
-      
+    created: async function() {
+      this.allCompleted = !this.groceries ? true : false
     },
     updated: async function() {
       const editItemInput = document.getElementById('edit-item') || undefined
       if(editItemInput) editItemInput.focus()
 
-      // await this.checkIfAllCompleted()
-      // const addItemInput = document.getElementById('add-item') || undefined
-      // addItemInput ? addItemInput.focus() : editItemInput.focus()
-      
+      await this.checkCompleted()
     },
     firebase: {
-      groceries: dbGroceriesRef.orderByChild('order'),
-      completedGroceries: dbCompletedGroceriesRef
+      groceries: dbGroceriesRef.orderByChild('order')
     },
     methods: {
-      async updateItem() {
-        if(await this.inputNotOk(this.itemToEdit.name)) return
-        dbGroceriesRef.child(this.itemToEdit['.key']).update({ name: this.itemToEdit.name })
-        this.isEditing = false
-        this.itemToEdit = {}
-      },
       async addItem() {
         if(await this.inputNotOk(this.itemToAdd)) return
         dbGroceriesRef.push({
@@ -111,6 +101,12 @@
         })
         this.itemToAdd = ''
         document.getElementById('add-item').focus()
+      },
+      async updateItem() {
+        if(await this.inputNotOk(this.itemToEdit.name)) return
+        dbGroceriesRef.child(this.itemToEdit['.key']).update({ name: this.itemToEdit.name })
+        this.isEditing = false
+        this.itemToEdit = {}
       },
       async inputNotOk(name) {
         if(this.nameTooShort(name)) {
@@ -131,25 +127,11 @@
         const exists = await this.groceries.filter(item => item.name === name)
         return !!exists.length
       },
-      async setItemCompleted(id) {
-        //TODO
-        this.groceries = this.groceries.reduce((acc, item) => 
-          item._id === id ? 
-          acc.concat(Object.assign({}, item, {completed: true})) : 
-          acc.concat(item),[]
-        )
-        await this.updateOrder()
-        
+      async setItemCompleted(key) {
+        dbGroceriesRef.child(key).update({ completed: true })
       },
-      async setItemNotCompleted(id) {
-        //TODO
-        this.groceries = this.groceries.reduce((acc, item) => 
-          item._id === id ? 
-          acc.concat(Object.assign({}, item, {completed: false})) : 
-          acc.concat(item),[]
-        )
-        await this.updateOrder()
-        
+      async setItemNotCompleted(key) {
+        dbGroceriesRef.child(key).update({ completed: false })
       },
       async editItem(item) {
         this.isEditing = true
@@ -160,25 +142,28 @@
       },
       removeItem(key) {
         dbGroceriesRef.child(key).remove()
-        this.updateOrder()
+      },
+      async checkCompleted() {
+        if(!this.groceries) return this.allCompleted = true
+        this.anyCompleted = await this.checkIfAnyCompleted()
+        this.allCompleted = await this.checkIfAllCompleted()
+      },
+      async checkIfAnyCompleted() {
+        const completed = await this.groceries.filter(item => item.completed)
+        return !!completed.length
       },
       async checkIfAllCompleted() {
-        //TODO: CHeck if this works properly
         const notCompleted = await this.groceries.filter(item => !item.completed)
-        this.allCompleted = notCompleted.length === 0
+        const allCompleted = notCompleted.length ? false : true
+        return allCompleted
       },
       clearCompleted() {
-        //TODO
-        this.completedGroceries = []
-        this.groceries = this.groceries.filter(item => !item.completed )
-        this.updateOrder()
+        this.groceries.map(item => item.completed && dbGroceriesRef.child(item['.key']).remove())
+        this.anyCompleted = false
       }
     }
   }
 
-  /***************** Firebase functions *****************/
-  // dbGroceriesRef.on('value', snap => groceries = snap.val())
-  /******************************************************/
 </script>
 
 <style>
